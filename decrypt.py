09bdd532a550389d90b1a7f48cd52ae31a8482d1
Susmit Kumar Mishra <susmitmishra125@gmail.com>
2022-01-11 01:07:40 +0530

    converted to csv format

--- decrypt.py
+++ decrypt.py
@@ -1,17 +1,17 @@
 from cryptography.fernet import Fernet
+import pandas as pd
 # read the file
-f = open("key.txt", "r")
-lines = f.readlines()
-f.close()
 def get_key():
-    key= open("pass.txt","rb").read()
+    key= open("key.txt","rb").read()
     return key
-for sent in lines:
-	x=sent.split(' ')
-	domain = x[0]
-	username = x[1]
-	# read the key from the file
-	key= get_key()
-	a = Fernet(key)
-	password = a.decrypt(x[2].encode('utf-8'))
-	print(domain,username,password.decode('utf-8'))
+# loop the pass.csv file
+def decrypt():
+		df = pd.read_csv('pass.csv')
+		key = get_key()
+		a = Fernet(key)
+		row_num = input('enter the row number you want to decrypt: ')
+		encrypted_msg = df.loc[int(row_num),'password']
+		decrypted_msg = a.decrypt(encrypted_msg.encode("utf-8"))
+		# print username and decrypted message
+		print(df.loc[int(row_num),'username'], decrypted_msg.decode("utf-8"))
+decrypt()
--- encrypt.py
+++ encrypt.py
@@ -1,32 +1,58 @@
 from cryptography.fernet import Fernet
-def genewrite_key():
-    key= Fernet.generate_key()
-    with open("pass.txt","wb") as key_file:
-        key_file.write(key)
+import pandas as pd
+import os
+from datetime import datetime
+
+
+def generate_key():
+	# generate key and save it to key.txt
+	if os.path.exists('key.txt'):
+		print('key.txt already exists press y to overwrite the current key')
+		flag = input()
+		if flag.lower() != 'y':
+			return
+	key = Fernet.generate_key()
+	with open("key.txt", "wb") as key_file:
+			key_file.write(key)
+
 def get_key():
-    key= open("pass.txt","rb").read()
-    return key
+		if not os.path.exists('key.txt'):
+			generate_key()
+		key= open("key.txt","rb").read()
+		return key
 
 
 def encrypt():
 	# takes a string and returns a string
 	msg=input("Enter the message you want to ENCRYPT : ")
-	text = msg.encode()
+	text = msg.encode("utf-8")
 	# read from key.txt
-	key= get_key()
+	key = get_key()
 	a = Fernet(key)
 
 	encrypted_msg= a.encrypt(text)
 	print(encrypted_msg)
 	flag =  input('should i append this key to the file? (y/n)')
 	if flag == 'y':
+		# check if pass.csv exists else create
+		if not os.path.exists('pass.csv'):
+			df = pd.DataFrame(columns=['date','time','domain','username','password'])
+			df.to_csv('pass.csv', index=False)
+		df = pd.read_csv('pass.csv')
+
 		print('enter the domain name or website name')
 		domain = input()
-		f = open("key.txt", "a")
+		# f = open("pass.txt", "a")
 		print('enter the username')
 		username = input()
-		f.write(domain+' '+username+' '+encrypted_msg.decode("utf-8")+'\n')
-		f.close()
-		print('key added to file')
-genewrite_key()
-# encrypt()
+		# append domain username and encrypted message to pass.csv
+		now = datetime.now() # current date and time
+		date = now.strftime("%d/%m/%Y")
+		time = now.strftime("%H:%M:%S")
+		df.loc[len(df)] = [date,time,domain,username,encrypted_msg.decode("utf-8")]
+		df.to_csv('pass.csv', index=False)
+		print('saved encrypted message to pass.csv')
+if __name__ == '__main__':
+	encrypt()
+	# uncomment the following line to generate a key or overwrite the current key
+	# generate_key()
